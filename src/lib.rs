be21e89f388399450b5b2b4c65862aa0354f4da4
Nat Quayle Nelson <natquaylenelson@gmail.com>
2018-07-27 11:09:31 -0400

    Adding subprocess, porting a function

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,17 @@
+use std::env;
+
 // TODO port the code from this file of the Git source:
 // https://github.com/git/git/blob/936d1b989416a95f593bf81ccae8ac62cd83f279/editor.c
+
+fn is_terminal_dumb() -> bool {
+    match env::var("TERM") {
+        Err(_) => true,
+        Ok("dumb") => true,
+        Ok(_) => false,
+    }
+}
+
+
 
 #[cfg(test)]
 mod tests {
