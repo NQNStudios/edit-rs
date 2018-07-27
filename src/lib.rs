c54141ac53237906b3b8e85fdd65d92a71eb28b3
Nat Quayle Nelson <natquaylenelson@gmail.com>
2018-07-26 20:31:34 -0400

    Crate initialization, TODO note

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+// TODO port the code from this file of the Git source:
+// https://github.com/git/git/blob/936d1b989416a95f593bf81ccae8ac62cd83f279/editor.c
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
